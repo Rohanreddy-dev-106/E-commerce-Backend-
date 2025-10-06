9d22a50fa78c2cab4d0a509e486087f04868532d
Rohanreddy-dev-106 <rohan.reddy.dev106@gmail.com>
2025-10-06 16:50:41 +0530

    Project Started

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const server=express();
+
+
+
+
+
+
+
+export{server}
---
+++ server.js
@@ -0,0 +1,5 @@
+import { server } from "./index.js";
+
+const PORT =8080;
+
+server.listen(PORT,()=>console.log(`Server is started at POTE ${PORT}......`));
